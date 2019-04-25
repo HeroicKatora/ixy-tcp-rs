792a332167071f8a3c37329f1da173762e162c6b
Andreas Molzer <andreas.molzer@gmx.de>
2019-04-26 01:40:03 +0200

    Finalize linux comparison version
    Removes epoll completely in favour of simply busy polling and takes the
    forwarding address as another parameter from the command line. Both
    parallel similar decisions in the udp example.

--- compare/linux_udp.c
+++ compare/linux_udp.c
@@ -10,16 +10,16 @@
 #define MAX_EVENTS (128)
 #define BUF_LEN (65535)
 
-int init_epoll(int sock_a, int sock_b);
 int init_udp_sock(const char* bind_addr);
-void register_epoll(int epoll_fd, int read_fd);
-void forward_messages(int from, int to);
+void init_target_addr(struct sockaddr_in* sock, const char* addr);
+void forward_messages(int from, int to, const struct sockaddr_in* via);
 
 int main(int argc, char** argv) {
-	struct epoll_event events[MAX_EVENTS];
-	int sock_a, sock_b, fd_epoll, nfds;
+	int sock_a, sock_b;
 
-	if(argc != 3) {
+	struct sockaddr_in to_a, to_b;
+
+	if(argc != 5) {
 		printf("Usage: linux_udp <bind_addr_1> <bind_addr_2>\n");
 		exit(1);
 	}
@@ -27,47 +27,22 @@ int main(int argc, char** argv) {
 	printf("[+] Startup\n");
 	sock_a = init_udp_sock(argv[1]);
 	sock_b = init_udp_sock(argv[2]);
-	fd_epoll = init_epoll(sock_a, sock_b);
+	init_target_addr(&to_a, argv[3]);
+	init_target_addr(&to_b, argv[4]);
 
 	printf("[+] Initialized\n");
 
 	for(;;) {
-		nfds = epoll_wait(fd_epoll, events, MAX_EVENTS, -1);
-		if(nfds < 0) {
-			perror("Failed to epoll_wait");
-			exit(1);
-		}
-
-		for(int i = 0; i < nfds; i++) {
-			if(events[i].data.fd == sock_a) {
-				forward_messages(sock_a, sock_b);
-			} else if(events[i].data.fd == sock_b) {
-				forward_messages(sock_b, sock_a);
-			}
-		}
+		forward_messages(sock_a, sock_b, &to_a);
+		forward_messages(sock_b, sock_a, &to_b);
 	}
-}
-
-int init_epoll(int sock_a, int sock_b) {
-	int fd_epoll;
-
-	fd_epoll = epoll_create1(0);
-	if(fd_epoll < 0) {
-		perror("Failed to create epoll");
-		exit(1);
-	}
-
-	register_epoll(fd_epoll, sock_a);
-	register_epoll(fd_epoll, sock_b);
-
-	return fd_epoll;
 }
 
 int init_udp_sock(const char* bind_addr) {
 	struct sockaddr_in addr;
 	int sock;
 
-	sock = socket(AF_INET, SOCK_DGRAM | SOCK_NONBLOCK, 0);
+	sock = socket(AF_INET, SOCK_DGRAM | SOCK_NONBLOCK, IPPROTO_UDP);
 	if(sock < 0) {
 		perror("Failed to create socket");
 		exit(1);
@@ -92,26 +67,23 @@ int init_udp_sock(const char* bind_addr) {
 	return sock;
 }
 
-void register_epoll(int fd_epoll, int fd_read) {
-	struct epoll_event ev;
-
-	ev.events = EPOLLIN;
-	ev.data.fd = fd_read;
-
-	if(epoll_ctl(fd_epoll, EPOLL_CTL_ADD, fd_read, &ev) < 0) {
-		perror("Failed to register socket for read");
+void init_target_addr(struct sockaddr_in* sock, const char* addr) {
+	sock->sin_family = AF_INET;
+	sock->sin_port = htons(1234);
+	if(inet_aton(addr, &sock->sin_addr) == 0) {
+		perror("Not a valid address");
 		exit(1);
 	}
 }
 
-void forward_messages(int from, int to) {
+void forward_messages(int from, int to, const struct sockaddr_in* via) {
 	static char BUF[BUF_LEN];
 
 	struct sockaddr_in addr;
 	socklen_t addr_len;
 	ssize_t len;
 
-	for(;;) {
+	for(int i = 0;i < 128;i++) {
 		addr_len = sizeof(struct sockaddr_in);
 		len = recvfrom(from, BUF, BUF_LEN, 0,
 			(struct sockaddr *) &addr, &addr_len);
@@ -123,7 +95,10 @@ void forward_messages(int from, int to) {
 		}
 
 		// We do not care about success. Failed messages are simply dropped.
-		sendto(to, BUF, len, 0,
-			(struct sockaddr *) &addr, addr_len);
+		if(sendto(to, BUF, len, 0,
+			(struct sockaddr *) via, sizeof(*via)) < 0) {
+			perror("Failed to send");
+			// exit(1);
+		}
 	}
 }
