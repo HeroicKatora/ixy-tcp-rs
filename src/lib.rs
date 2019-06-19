fc7fe63c43c528acd59c90bbd8831fa94121d380
Andreas Molzer <andreas.molzer@gmx.de>
2019-06-19 16:35:31 +0200

    Rework to ethox
    The udp example is currently missing as it needs to be recoded from
    scratch, except for the input format.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,15 @@
-use std::collections::VecDeque;
+use std::collections::{VecDeque, vec_deque::IterMut};
 use std::rc::Rc;
 
 use ixy::IxyDevice;
-use ixy::memory::{self, Mempool, Packet};
+use ixy::memory::{self, Mempool, Packet as IxyPacket};
 
-use smoltcp::{Error as NetError, Result as NetResult};
-use smoltcp::time::Instant;
-use smoltcp::phy;
+use ethox::layer::Result as NicResult;
+use ethox::nic;
+use ethox::wire;
+use ethox::time::Instant;
 
-/// A generic ixy device as a smoltcp phy device.
+/// A generic ixy device as an ethox phy device.
 ///
 /// Newtype wrapper so that this struct can live in an external crate instead of ixy-rs itself.
 pub struct Phy<D> {
@@ -16,17 +17,26 @@ pub struct Phy<D> {
     device: D,
 
     /// Packets to be processed in receive.
-    rx_queue: VecDeque<Packet>,
+    rx_queue: VecDeque<IxyPacket>,
 
     /// Packets which can be used for sending.
-    tx_empty: VecDeque<Packet>,
+    tx_empty: VecDeque<IxyPacket>,
 
     /// Packets ready for sending but waiting to be batched.
-    tx_queue: VecDeque<Packet>,
+    tx_queue: VecDeque<IxyPacket>,
 
     /// Memory pool to use for allocation.
     pool: Rc<Mempool>,
 }
+
+#[derive(Clone, Copy, Debug)]
+pub struct Handle {
+    queued: bool,
+    timestamp: Instant,
+}
+
+#[repr(transparent)]
+pub struct Packet(IxyPacket);
 
 impl<D> Phy<D> {
     const BATCH_SIZE: usize = 32;
@@ -51,7 +61,9 @@ impl<D> Phy<D> {
     pub fn into_inner(self) -> D {
         self.device
     }
+}
 
+impl<D: IxyDevice> Phy<D> {
     /// Empty the send buffer.
     ///
     /// The network stack of `smoltcp` only gives an interface for sending single packets. In order
@@ -59,160 +71,155 @@ impl<D> Phy<D> {
     /// specific size. Just call this periodically, e.g. each loop iteration.
     ///
     /// Returns the number of packets sent due to this call to flush.
-    pub fn flush(&mut self) -> usize where D: IxyDevice {
+    pub fn flush(&mut self) -> usize {
         self.device.tx_batch(0, &mut self.tx_queue)
     }
 
-    fn rx(&mut self) -> Option<Packet> where D: IxyDevice {
+    fn get_rx(&mut self) -> IterMut<IxyPacket> {
         if self.rx_queue.is_empty() {
             self.device.rx_batch(0, &mut self.rx_queue, Self::BATCH_SIZE);
         }
 
         // Receive in correct time order.
-        self.rx_queue.pop_front()
+        self.rx_queue.iter_mut()
     }
 
-    fn unrx(&mut self, packet: Packet) {
-        self.rx_queue.push_front(packet)
-    }
-
-    fn tx(&mut self) -> Option<Packet> where D: IxyDevice {
+    fn get_tx(&mut self) -> IterMut<IxyPacket> {
         if self.tx_empty.is_empty() {
             let max_size = self.pool.entry_size();
             memory::alloc_pkt_batch(&self.pool, &mut self.tx_empty, Self::BATCH_SIZE, max_size);
         }
 
         // Back is the last sent packet, best chance to still be in TLB/mmio cache?
-        self.tx_empty.pop_back()
-    }
-
-    fn untx(&mut self, packet: Packet) {
-        assert!(packet.len() == self.pool.entry_size());
-        self.tx_empty.push_back(packet)
+        self.tx_empty.iter_mut()
     }
 }
 
-/// Private trait implementing batched sending.
-///
-/// Used by `TxToken` as an abstraction so that it does not require the type implementing
-/// `IxyDevice` in its interface.
-trait Sender {
-    /// Add a packet for sending.
-    ///
-    /// It might not send it immediately to batch multiple calls.
-    fn enqueue(&mut self, packet: Packet) -> NetResult<()>;
-}
-
-pub struct RxToken {
-    packet: Packet,
-}
-
-pub struct TxToken<'a> {
-    packet: Packet,
-    queue: &'a mut Sender,
-}
-
-impl<'a, D: IxyDevice> phy::Device<'a> for Phy<D> {
-    type RxToken = RxToken;
-    type TxToken = TxToken<'a>;
-
-    fn receive(&'a mut self) -> Option<(RxToken, TxToken)> {
-        match (self.rx(), self.tx()) {
-            (Some(rx), Some(tx)) => {
-                Some((RxToken::from(rx), TxToken::from(tx, self)))
-            },
-            (Some(rx), None) => {
-                self.unrx(rx);
-                None
-            },
-            (None, Some(tx)) => {
-                self.untx(tx);
-                None
-            },
-            (None, None) => None,
+impl Handle {
+    fn new(now: Instant) -> Self {
+        Handle {
+            queued: false,
+            timestamp: now,
         }
     }
+}
 
-    fn transmit(&'a mut self) -> Option<TxToken> {
-        match self.tx() {
-            None => None,
-            Some(tx) => Some(TxToken::from(tx, self))
-        }
-    }
-
-    fn capabilities(&self) -> phy::DeviceCapabilities {
-        let mut capabilities = phy::DeviceCapabilities::default();
-        capabilities.checksum.udp = phy::Checksum::None;
-        capabilities.max_transmission_unit = self.pool.entry_size();
-        // FIXME: no idea what this exactly does. May need to return the allocation size of the
-        // buffer here.
-        capabilities.max_burst_size = None;
-        capabilities
+impl Packet {
+    fn from_mut(ixy: &mut IxyPacket) -> &mut Self {
+        // Safety: marked with `repr(transparent)`. Doesn't change mutability.
+        unsafe { core::mem::transmute(ixy) }
     }
 }
 
-impl<'a, D: IxyDevice> Sender for Phy<D> {
-    fn enqueue(&mut self, packet: Packet) -> NetResult<()> {
-        self.tx_queue.push_back(packet);
-        if self.tx_queue.len() >= Self::BATCH_SIZE {
-            self.flush();
-        }
-        Ok(())
-    }
-}
+impl<D: IxyDevice> nic::Device for Phy<D> {
+    type Handle = Handle;
+    type Payload = Packet;
 
-impl RxToken {
-    /// Create an rx token.
-    ///
-    /// Not public through `convert::From` as it should only be created by `Phy`.
-    pub(crate) fn from(packet: Packet) -> Self {
-        RxToken { packet }
+    fn personality(&self) -> nic::Personality {
+        nic::Personality::baseline()
     }
-}
 
-impl<'a> TxToken<'a> {
-    /// Create a tx token.
-    ///
-    /// Not public through `convert::From` as it should only be created by `Phy` and we may have
-    /// additional invariants.
-    pub(crate) fn from(packet: Packet, queue: &'a mut Sender) -> Self {
-        TxToken { packet, queue }
-    }
-}
-
-impl phy::RxToken for RxToken {
-    fn consume<R, F>(self, _ts: Instant, f: F) -> NetResult<R>
-        where F: FnOnce(&[u8]) -> NetResult<R>
+    fn tx(&mut self, max: usize, mut sender: impl nic::Send<Self::Handle, Self::Payload>)
+        -> NicResult<usize>
     {
-        f(&self.packet)
-    }
-}
+        let now = Instant::now();
+        let mut handles = [Handle::new(now); 32];
+        
+        // Provide packets to the sender.
+        let packets = self
+            .get_tx()
+            .zip(handles.iter_mut())
+            .map(|(packet, handle)| {
+                nic::Packet {
+                    handle,
+                    payload: Packet::from_mut(packet),
+                }
+            })
+            .take(max);
 
-impl<'a> phy::TxToken for TxToken<'a> {
-    fn consume<R, F>(mut self, _ts: Instant, length: usize, f: F) -> NetResult<R>
-        where F: FnOnce(&mut [u8]) -> NetResult<R>
+        let count = packets.len();
+        sender.sendv(packets);
+
+        // Gather potentially sent and step through those that were marked as sent.
+        let tx_queue = &mut self.tx_queue;
+        let sent = self.tx_empty
+            .drain(..count)
+            .zip(handles.iter())
+            .fold(0, |count, (packet, handle)| {
+                count + if handle.queued {
+                    tx_queue.push_back(packet);
+                    1
+                } else {
+                    // Drops packet
+                    0
+                }
+            });
+        self.flush();
+        Ok(sent)
+    }
+
+    fn rx(&mut self, max: usize, mut receptor: impl nic::Recv<Self::Handle, Self::Payload>)
+        -> NicResult<usize>
     {
-        if self.packet.len() <= length {
-            // Assume that the packet was chosen as long as possible.  This needs to change if we
-            // allow using a received packet directly but the packet allocator makes them as long
-            // as possible, I think.
-            return Err(NetError::Illegal)
-        }
+        let now = Instant::now();
+        let mut handles = [Handle::new(now); 32];
 
-        // resize the packet to the requested size.
-        self.packet.truncate(length);
+        // Provide packets to the receiver.
+        let packets = self
+            .get_rx()
+            .zip(handles.iter_mut())
+            .map(|(packet, handle)| {
+                nic::Packet {
+                    handle,
+                    payload: Packet::from_mut(packet),
+                }
+            })
+            .take(max);
+        let count = packets.len();
+        receptor.receivev(packets);
 
-        // TODO: evaluate if we should initialize memory in `packet` before. Currently, it may
-        // still contain the contents of a previous packet (but not actually uninitialized
-        // content, still may be a security vulnerability as this basically bypasses the borrow
-        // checker as a custom allocator).
-        assert!(self.packet.len() == length);
-        let r = f(&mut self.packet[..])?;
+        // Gather those sent again immediately
+        let tx_queue = &mut self.tx_queue;
+        let sent = self.tx_empty
+            .drain(..count)
+            .zip(handles.iter())
+            .fold(0, |count, (packet, handle)| {
+                count + if handle.queued {
+                    tx_queue.push_back(packet);
+                    1
+                } else {
+                    // Drops packet
+                    0
+                }
+            });
+        self.flush();
+        Ok(sent)
 
-        self.queue.enqueue(self.packet).map(move |_| r)
     }
 }
 
-#[cfg(test)]
-mod tests {
+impl nic::Handle for Handle {
+    fn queue(&mut self) -> NicResult<()> {
+        Ok(self.queued = true)
+    }
+
+    fn info(&self) -> &nic::Info {
+        self
+    }
+}
+
+impl nic::Info for Handle {
+    fn timestamp(&self) -> Instant {
+       self.timestamp 
+    }
+
+    fn capabilities(&self) -> nic::Capabilities {
+        nic::Capabilities::no_support()
+    }
+}
+
+impl wire::Payload for Packet {
+    fn payload(&self) -> &wire::payload {
+        self.0.as_ref().into()
+    }
 }
